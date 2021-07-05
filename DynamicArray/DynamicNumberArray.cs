d7c047e4f7ea3befc028bf623619a960b1e64206
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-07-05 16:01:06 +0530

    Add files via upload

---
+++ DynamicArray/DynamicNumberArray.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArrayProgram
+{
+    public class DynamicNumberArray
+    {
+        public  int[] buffer;
+
+        public DynamicNumberArray()
+        {
+            buffer = new int[5];
+        }
+        public DynamicNumberArray(int initialSize)
+        {
+            buffer = new int[initialSize];
+        }
+
+        public int this[int index]
+        {
+            set
+            {
+               if(index>=buffer.Length)
+                {
+                    ResizeMethod.Resize(ref buffer, buffer.Length+1);
+                }
+
+                buffer[index] = value;
+            }
+            get
+            {
+                return buffer[index];
+            }
+        }   
+    }
+}
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DynamicArray/ResizeMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArrayProgram
+{
+    class ResizeMethod
+    {
+          public static void Resize(ref int[] oldarray, int newSize)
+        {
+                      
+            int[] newarray= new int[newSize];
+            Array.Copy(oldarray, newarray, oldarray.Length);
+            oldarray = new int[newSize];
+            Array.Copy(newarray, oldarray, newSize);
+           
+        }
+    }
+}
